d3a832853c6dacca7bdf92c810a291a3730dc397
Christopher Rodger <christopher.rodger@gmail.com>
2025-09-01 13:37:25 +0000

    preprocessor examples

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -2,9 +2,23 @@
 // Exercise 02_06
 // Preprocessor directives, by Eduardo Corpeño 
 
-#include <iostream>
+#include <iostream> // this is a preprocessor directive
+#include <string>
+#include <cstdint>
+
+#define CAPACITY 5000 // act like find and replace, don't end with semicolon
+#define DEBUG //Useful for debugging using ifdef preprocessor
 
 int main(){
+    int32_t large = CAPACITY;
+    uint8_t small = 37;
+
+#ifdef DEBUG
+    std::cout << "[About to perform the addition]" << std::endl;
+#endif
+
+    large += small; // Shorthand for "large = large + small;"
+    std::cout << "The large integer is " << large << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
