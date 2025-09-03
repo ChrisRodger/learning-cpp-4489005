71b5ca9be3683c89a08ac94e2e57742faa6c3d1a
Christopher Rodger <christopher.rodger@gmail.com>
2025-09-03 14:26:15 +0000

    Type casting demo

--- src/Ch02/02_10b/CodeDemo.cpp
+++ src/Ch02/02_10b/CodeDemo.cpp
@@ -6,7 +6,18 @@
 #include <cstdint>
 
 int main(){
-    
+    float flt;
+    int32_t sgn;
+    uint32_t unsgn;
+
+    flt = -7.66; // without trailing f, is a double constant. IDE knows this - hover over value.
+    sgn = flt; // value is truncated, float to int
+    unsgn = sgn; // twos complement value of 7, 2^32 - 7
+
+    std::cout << " float: " << flt << std::endl;
+    std::cout << " int32: " << sgn << std::endl;
+    std::cout << "uint32: " << unsgn << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
