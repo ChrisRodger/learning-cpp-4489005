6815cb744e83f6f5a2888fd591e87a7556f76890
Christopher Rodger <christopher.rodger@gmail.com>
2025-09-03 16:55:13 +0000

    Enumeration demo

--- src/Ch02/02_12b/CodeDemo.cpp
+++ src/Ch02/02_12b/CodeDemo.cpp
@@ -5,14 +5,28 @@
 #include <iostream>
 #include <cstdint>
 
-enum cow_purpose {dairy, meat, hide, pet};
+//enum cow_purpose {dairy, meat, hide, pet}; // c-style enumeration
+//enum grocery_section {canned, frozen, meat, laundry, dairy, bakery}; // can't redefine
+
+// enum classes are strongly typed
+
+enum class cow_purpose {dairy, meat, hide, pet}; // c++ style enumeration
+enum class grocery_section {canned, frozen, meat, laundry, dairy, bakery};
 
 int main(){
-    int a;
+    int meat = 8;
+    cow_purpose a;
+    grocery_section b;
+    grocery_section c;
 
-    a = meat;
+    a = cow_purpose::meat;
+    b = grocery_section::meat;
+    c = grocery_section::bakery;
 
-    std::cout << "a = " << a << std::endl;
+    // cout is not defined for cow_purpose type, therefore need to cast a to int
+    std::cout << "a = " << (int) a << std::endl;
+    std::cout << "b = " << (int) b << std::endl;
+    std::cout << "c = " << (int) c << std::endl;
 
     std::cout << std::endl << std::endl;
     return (0);
