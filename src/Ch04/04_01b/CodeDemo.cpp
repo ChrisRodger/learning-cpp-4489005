76e957bdb16551a101ccc02d6235cfe96d199d3d
Christopher Rodger <christopher.rodger@gmail.com>
2025-09-18 13:31:20 +0000

    If statements demo

--- src/Ch04/04_01b/CodeDemo.cpp
+++ src/Ch04/04_01b/CodeDemo.cpp
@@ -5,6 +5,32 @@
 #include <iostream>
 
 int main(){
+    int a = 1023;
+    bool flag = false;
+    char letter = 'd';
+
+    // if a block of code consists of one line, no need for curly brackets
+
+    if (a > 1000)
+        std::cout << "Warning: a is over 1000." << std::endl;
+
+    // 0 is false in boolean, so no need to express explicitly
+    // (a % 2) == 0 can be written a % 2
+    if (a % 2)
+        std::cout << "a is odd" << std::endl;
+    else
+        std::cout << "a is even" << std::endl;
+
+    std::cout << "The letter " << letter << " is ";
+    if(letter != 'a' && letter != 'e' && letter != 'i' && letter != 'o' && letter != 'u' &&
+       letter != 'A' && letter != 'E' && letter != 'I' && letter != 'O' && letter != 'U')
+       std::cout << "not ";
+    std::cout << "a vowel" << std::endl;
+
+    if (flag)
+        std::cout << "The flag is true!" << std::endl;
+    else
+        std::cout << "The flag is false!" << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
